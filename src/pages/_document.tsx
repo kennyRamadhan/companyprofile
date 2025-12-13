7bb0d18e9e5c354e3f0ab4f7de7f6f016c0dc7e7
Kenny Ramadhan <VM915021@maybankcoid.onmicrosoft.com>
2025-12-14 03:38:23 +0700

    Add identity widget to root for invite verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="id">
+      <Head>
+        {/* 1. Masukkan Widget Script di sini */}
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
+        
+        {/* 2. Masukkan Redirect Script di bawah sini */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+            `,
+          }}
+        />
       </body>
     </Html>
-  );
+  )
 }
